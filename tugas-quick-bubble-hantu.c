604b1c2ebbf4495261d21d8564f5eb92e10be95c
nabilah nugroho <boredluckykid@gmail.com>
2026-04-13 00:09:02 +0700

    added quick sort function

--- tugas-quick-bubble-hantu.c
+++ tugas-quick-bubble-hantu.c
@@ -5,6 +5,41 @@ int m, n;
 int hantu[serem];
 int angka[100];
 
+void tuker(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int posisi(int angka[], int low, int high) {
+    int pivot = angka[high]; 
+    int i = (low - 1); 
+
+    for (int j = low; j < high; j++) {
+        // asc
+        if (angka[j] < pivot) {
+            i++;
+            tuker(&angka[i], &angka[j]);
+        }
+    }
+    tuker(&angka[i + 1], &angka[high]);
+    return (i + 1);
+}
+
+// Fungsi rekursif Quick Sort
+void quickSort(int angka[], int low, int high) {
+    if (low < high) {
+        int pi = posisi(angka, low, high);
+
+        quickSort(angka, low, pi - 1);  // bagian kiri pivot
+        quickSort(angka, pi + 1, high); // bagian kanan pivot
+    }
+    printf("hasil sorting quick sort (asc): \n");
+    for(int i = 0; i < n; i++){
+        printf("%d ", angka[i]);
+    }
+}
+
 int hantuIni(int x){
     for(int i=0; i<m; i++){
         if(x==hantu[i])return 1;
@@ -12,19 +47,18 @@ int hantuIni(int x){
     return 0;
 }
 
-int bubbleSort(int angka[], int size, int hantu[]){
+void bubbleSort(int angka[], int size, int hantu[]){
     int temp, i, j;
     for(i=0; i<size-1; i++){
         for(j=0; j<size-1-i; j++){
             if(angka[j]<angka[j+1]){
-                temp= angka[j]; angka[j]=
-                angka[j+1];angka[j+1]= temp;      
+                tuker(&angka[j], &angka[j+1]);    
             }
         }
     }
     printf("hasil sorting bubble desc: \n");
     for(int i = 0; i < n; i++){
-        if(hantuIni(hantu[i])){
+        if(hantuIni(angka[i])){
             printf("  ");
         }
         else{
@@ -44,6 +78,7 @@ void input() {
         for(int i = 0; i < n; i++){
             printf("angka ke-%d: ", i+1); scanf("%d", &angka[i]);
         }
+        quickSort(angka, 0, n-1);
         bubbleSort(angka, n, hantu);
         }
     else {
@@ -57,4 +92,4 @@ void inputHantu(){
 int main() {
     input();
     return 0;
-}
+}   
